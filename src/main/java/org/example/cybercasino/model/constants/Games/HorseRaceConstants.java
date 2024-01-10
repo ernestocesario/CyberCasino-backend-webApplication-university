9001063902189a5f4b1e652e444ef01f1db770a7
domenico3622 <128099390+domenico3622@users.noreply.github.com>
2024-01-10 11:14:56 +0100

    aggiunta qualche costante per HorseRaceConstants

--- src/main/java/org/example/cybercasino/model/constants/Games/HorseRaceConstants.java
+++ src/main/java/org/example/cybercasino/model/constants/Games/HorseRaceConstants.java
@@ -1,6 +1,15 @@
 package org.example.cybercasino.model.constants.Games;
 
 public class HorseRaceConstants {
+    public final int numberOfHorses = 4;
+    public final String[] elements = {
+            "horse1",
+            "horse2",
+            "horse3",
+            "horse4"
+    };
+    public final int winningPercentage = 25;
+    public final int betMultiplier = 4;
     private static HorseRaceConstants instance;
 
     public static HorseRaceConstants getInstance() {
