5e92a72d336240a2a27ee45a3868bb9d2b829c81
fefe202 <dfrfrc02e23c349m@studenti.unical.it>
2024-01-19 15:11:59 +0100

    Refactoring getToken + migliorato CSS Guess The Card

--- src/main/resources/static/roulette/javaswheel.js
+++ src/main/resources/static/roulette/javaswheel.js
@@ -6,7 +6,7 @@ import {GameType} from "../js/gameType.js";
 const urlParams = new URLSearchParams(window.location.search);
 let token = getToken(urlParams);
 if (token==null){
-    alert('Non siamo riusciti a verificare il tuo account, verrai reindirizzato alla pagina di login');
+    alert('We were unable to verify your account, you will be redirected to the login page.');
     resetGame();
 }
 
